caee5547c83f01b3a8ce20e2004c0424d27351e7
Vipul Negi <vipulnegi@Vipuls-MacBook-Air.local>
2023-09-11 11:58:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from 'react';
+import { click } from "@testing-library/user-event/dist/click";
 
 function App() {
   return (
@@ -17,8 +19,72 @@ function App() {
         >
           Learn React
         </a>
+        <ButtonFunction />
+        <Profile />
+        <ShoppingList />
       </header>
     </div>
+  );
+}
+
+function ButtonFunction() {
+  //const [count, setCount] = useState(0);
+
+  function handleClick() {
+   // alert(`You clicked the button. Count: ${count}`);
+   // setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <button className="Button" onClick={handleClick}>X</button>
+    </div>
+  );
+}
+
+
+
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
+}
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+      color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+    {product.title}
+    </li>
+  );
+  return (  
+    <ul className="ShoppingList">{listItems}</ul>
   );
 }
 
